ccded65b56c6f3ada35fe226883a6cae41feab76
NizarMohd <mohdnizar97@gmail.com>
2022-01-20 04:31:42 +0800

    new commit

--- frontend/src/components/CreateNewPost.js
+++ frontend/src/components/CreateNewPost.js
@@ -1,48 +1,33 @@
-import React, { Component } from "react";
-class CreatePost extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            image: null
-        };
+import React, { Component , useState} from "react";
 
-        this.onImageChange = this.onImageChange.bind(this);
-    }
-
-    onImageChange = event => {
-        if (event.target.files && event.target.files[0]) {
-            let img = event.target.files[0];
-            this.setState({
-                image: URL.createObjectURL(img)
-            });
-        }
+const CreatePost = () => {
+    const [selectedFile, setSelectedFile] = useState();
+    const changeHandler = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+    const handleSubmission = () => {
     };
 
-    render() {
-        return (
-            <div>
-                <h1>New Post</h1>
-                <div className='parent'>
-                    <div className='child inline-block-child'>
-                        <img src="public/logo192.png" alt="profile_pic" width="50" height="50"/>
-                    </div>
-                    <div className='child inline-block-child'>
-                        <h3>Username</h3>
-                    </div>
+    return(
+        <div>
+            <h1>New Post</h1>
+            <div className='parent'>
+                <div className='child inline-block-child'>
+                    <img src="public/logo192.png" alt="profile_pic" width="50" height="50"/>
                 </div>
-                <div className='parent'>
-                    <div className='child inline-block-child-flex'>
-                        <img src={this.state.image} height={200} width={200} />
-                        <input type="file" name="myImage" onChange={this.onImageChange} />
-                    </div>
-                    <div className='child inline-block-child-flex'>
-                        <textarea type={"text"} rows={10} cols={50}/>
-                    </div>
+                <div className='child inline-block-child'>
+                    <h3>Username</h3>
                 </div>
-
             </div>
 
-        );
-    }
-}
+            <div>
+                <input type="file" name="file" onChange={changeHandler} />
+                <div className='child inline-block-child-flex'>
+                    <textarea type={"text"} rows={10} cols={50}/>
+                </div>
+                <button onClick={handleSubmission}>Submit</button>
+            </div>
+        </div>
+    )
+};
 export default CreatePost;
