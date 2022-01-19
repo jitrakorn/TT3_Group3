dcd6014287a988331300ecdaa7a9951924a0e26e
NizarMohd <mohdnizar97@gmail.com>
2022-01-20 04:04:31 +0800

    add changes to createnewpost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import CreatePost from "./components/CreateNewPost";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
+            <Route path="/createNewPost" element={<CreatePost />} />
         </Routes>
       </Router>
     </div>
