883d2c3afdea72529b80e203817233738d6efd30
Hakim <msnhbmf@gmail.com>
2022-01-20 12:36:15 +0800

    Added testing file and update postController
    added a testing file to test get API and updating the execute command

--- backend/postController.py
+++ backend/postController.py
@@ -7,8 +7,8 @@ api = Api(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
+app.config['MYSQL_PASSWORD'] = 'crazyZ1@boi'
+app.config['MYSQL_DB'] = 'socialmedia'
 
 mysql = MySQL(app)
 
@@ -17,12 +17,24 @@ class PostController(Resource):
 
     # gets to retrieve all POSTS from database
     def get(self):
+        # SELECT table1.id, table1.name, table2.id, table2.telephone
+        # FROM table1, table2
+        # WHERE table1.id = table2.id
 
-        return
+        cur = mysql.connection.cursor()
+        #cur.execute("SELECT post.Post_ID, post.Post_Title, post.Post_Description, post.Post_Image, liked_post.User_ID, liked_post.Post_ID, user.Name, user.User_ID, post_comment.Post_ID, post_comment.User_ID, post_comment.comment  FROM post, liked_post, user, post_comment WHERE post.Post_ID = liked_post.Post_ID && user.User_ID = liked_post.User_ID && user.User_ID = post_comment.User_ID && post.Post_ID = post_comment.Post_ID")
+
+        cur.execute("SELECT post.Post_ID, post.Post_Title, liked_post.User_ID, liked_post.Post_ID, user.Name, user.User_ID FROM post, liked_post, user WHERE post.Post_ID = liked_post.Post_ID && user.User_ID = liked_post.User_ID")
+        test = cur.fetchall()
+        print(test)
+        resp = jsonify(test)
+        resp.status_code = 200
+        print(resp)
+        return resp
 
     # puts create new post
-    def put(self):
-
+    def put(self, ):
+        # args = .parse_args()
         return
 
     def delete(self):
@@ -30,7 +42,7 @@ class PostController(Resource):
         return '', 204
 
 
-api.add_resource(PostController, "")
+api.add_resource(PostController, "/test")
 
 if __name__ == "__main__":
     app.run(debug=True)
